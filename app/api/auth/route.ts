42e189260229c04d5a4aceb999eb51c8565cb113
Azecko <samikarin24@gmail.com>
2026-05-26 10:43:42 +0200

    [fix] Redirecting to correct URL after login
    After logging in, if you tried to access to `/register` you should be redirected to `/register`
    
    May not work in production, changes are still possible.

--- app/api/auth/route.ts
+++ app/api/auth/route.ts
@@ -1,6 +1,14 @@
 import { signIn } from "@/auth"
 
+function getSafeCallbackUrl(callbackUrl: string | null) {
+	if (!callbackUrl || !callbackUrl.startsWith('/') || callbackUrl.startsWith('//')) {
+		return '/';
+	}
+
+	return callbackUrl;
+}
+
 export async function GET(req: Request) {
 	const searchParams = new URL(req.url).searchParams;
-	return signIn('microsoft-entra-id', { redirectTo: searchParams.get('callbackUrl') ?? '/' });
+	return signIn('microsoft-entra-id', { redirectTo: getSafeCallbackUrl(searchParams.get('callbackUrl')) });
 }
