c3799591154e06b2d996aa078cf6af33089c426b
Azecko <samikarin24@gmail.com>
2026-09-17 14:42:07 +0200

    [fix] correct URLs in middleware
    Some routes were missing, and also two API routes.

--- middleware.ts
+++ middleware.ts
@@ -11,19 +11,26 @@ export default async function middleware(req: NextRequest) {
 		return NextResponse.redirect(authUrl);
 	}
 
-	if (pathname === '/crep/register' || pathname.startsWith('/crep/register/')) {
+	// Routes available to anyone as long as they are logged in.
+	if (
+		pathname === '/crep/register' || pathname.startsWith('/crep/register/') ||
+		pathname === '/crep/exams' || pathname.startsWith('/crep/exams/') ||
+		pathname === '/teachers-exam-subscribe' || pathname.startsWith('/teachers-exam-subscribe/') ||
+		pathname === '/diff-exam-subscribe' || pathname.startsWith('/diff-exam-subscribe/')
+	) {
     	return NextResponse.next();
   	}
 
-	if (pathname === '/crep/exams' || pathname.startsWith('/crep/exams/')) {
-    	return NextResponse.next();
-  	}
-
+	// For all other routes, requiring at least access to the app (group access). If not, redirecting to `/403`.
 	if(!session.user.hasCrepAccess) {
 		return NextResponse.redirect(new URL("/403", req.url));
 	}
 
-	if (pathname === '/admin' || pathname.startsWith('/admin/')) {
+	// Routes available only to administrators of the app
+	if (
+		pathname === '/admin' || pathname.startsWith('/admin/') ||
+		pathname === '/exams' || pathname.startsWith('/exams/')
+	) {
 		if(!session.user.isAdmin) {
 			return NextResponse.redirect(new URL("/403", req.url));
 		}
@@ -35,5 +42,5 @@ export default async function middleware(req: NextRequest) {
 
 // All routes require login, except /api/auth, that is used to login... (and some static Next.js things)
 export const config = {
-  matcher: ["/((?!api/auth|api/upload-exam-files|api/crep/files|403|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/((?!api/auth|api/upload-exam-files|api/crep/files|api/cereal/diff-exams/get-bo-user|api/cereal/diff-exams/upload-absence|403|_next/static|_next/image|favicon.ico).*)"],
 }
